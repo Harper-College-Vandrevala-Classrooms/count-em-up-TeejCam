33e3a9067da4e91b6537864838d7e43667bab4d7
TeejCam <tjcamalan@gmail.com>
2024-09-14 18:16:43 -0500

    Made most of the functions

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -1,5 +1,69 @@
 package com.csc;
 
 public class GroceryCounter {
-  // Put your code here!
+  //tens ones tenths one-hundredths
+  int[] counter = new int[4];
+  public void increment_tens()
+  {
+    //counter[0]
+    counter[1] = counter[1] + 10;
+  }
+
+  public void increment_ones()
+  {
+    //counter[1]
+    // if
+    counter[1]++;
+  }
+
+  public void increment_tenths()
+  {
+    //counter[2]
+    if (counter[2] < 9){
+      counter[2]++;
+    }
+    else{
+      counter[1]++;
+      //increment_ones();
+      counter[2] = 0;
+    }
+  }
+
+  public void increment_hundredths()
+  {
+    //counter[3]++;
+
+    // check to see what counter[3] is equal to
+    // if it is less than 9, then increment and don't do anything else
+    // if it is equal to 9, then increment counter[2] and set counter[3] equal to zero
+    if (counter[3] < 9){
+      counter[3]++;
+    }
+    else {
+      increment_tenths();
+      counter[3] = 0;
+    }
+  }
+
+  public String total()
+  {
+
+    return "$" + counter[1] + "." + counter[2] + counter[3];
+    
+  }
+
+  /**public int number_of_overflows()
+  {
+
+  }
+
+  public void clear()
+  {
+    for (int i=0; i < counter.length; i++){
+      counter[i] = 0;
+    }
+  }*/
+
+  //do i need a static void main?
+  
 }
--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -14,11 +14,95 @@ public class TestGroceryCounter {
     counter = new GroceryCounter();
   }
 
-  // Put your tests here!
-
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void itReturnsZeroDollarsIfNoEntriesHaveBeenMade() {
+    assertEquals("$0.00", counter.total());
   }
 
+  @Test
+  void itRetunsOneCent() {
+    counter.increment_hundredths();
+    assertEquals("$0.01", counter.total());
+  }
+
+  @Test
+  void itReturnsTwoCents() {
+    counter.increment_hundredths();
+    counter.increment_hundredths();
+    assertEquals("$0.02", counter.total());
+  }
+
+  @Test
+  void itReturnsNineCents() {
+    for(int i = 0; i < 9; i++) {
+      counter.increment_hundredths();
+    }
+    assertEquals("$0.09", counter.total());
+  }
+
+  @Test
+  void itReturnsTenCents(){
+    for (int i=0; i<10; i++){
+      counter.increment_hundredths();
+    }
+    assertEquals("$0.10", counter.total());
+  }
+
+  @Test
+  void itReturnsElevenCents(){
+    for (int i=0; i<11; i++){
+      counter.increment_hundredths();
+    }
+    assertEquals("$0.11", counter.total());
+  }
+
+  @Test
+  void itIncrementsByTenCents() {
+    counter.increment_tenths();
+    assertEquals("$0.10", counter.total());
+  }
+
+  @Test
+  void itIncrementsByTenCentsTenTimes() {
+    for (int i=0; i<10; i++){
+      counter.increment_tenths();
+    }
+    assertEquals("$1.00", counter.total());
+  }
+
+  @Test
+  void itIncrementsByOneCentOneHundredTimes() {
+    for (int i=0; i<100; i++){
+      counter.increment_hundredths();
+    }
+    assertEquals("$1.00", counter.total());
+  }
+
+  @Test
+  void itIncrementsByOneDollar() {
+    counter.increment_ones();
+    assertEquals("$1.00", counter.total());
+  }
+
+  @Test
+  void itIncrementsByOneDollarTenTimes() {
+    for (int i=0; i<10; i++){
+      counter.increment_ones();
+    }
+    assertEquals("$10.00", counter.total());
+  }
+
+  @Test
+  void itIncrementsByOneDollarNinetyNineTimes() {
+    for (int i=0; i<99; i++){
+      counter.increment_ones();
+    }
+    assertEquals("$99.00", counter.total());
+  }
+
+  @Test
+  void itIncrementsByTenDollarsOneTime() {
+    counter.increment_tens();
+    assertEquals("$10.00", counter.total());
+  }
 }
